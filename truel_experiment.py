b043d03d786b147ba51ce0e7fca7ab64c379a1dc
Britt Binler <brittany.binler@gmail.com>
2015-12-09 19:01:29 -0500

    fixed graph

--- truel_experiment.py
+++ truel_experiment.py
@@ -8,12 +8,14 @@ import matplotlib.mlab as mlab
 import random
 import copy
 import operator
+import pprint as pprint
+# import michael
 
 results = {'Shooter 1' : 0, 'Shooter 2' : 0, 'Shooter 3' : 0} #value = number of times shooter wins
 shooters = {'Shooter 1' : 1.0/3, 'Shooter 2' : 2.0/3, 'Shooter 3' : 1} #value = shooter accuracy
 alive = copy.deepcopy(shooters)
 
-def get_best_shooter(shooters, shooter):
+def get_best_shooter(shooters, shooter): #shooter a string, shooters is a dict
 	'''Finds the most accurate living shooter who is not the person shooting. Returns the best shooter.'''
 	global alive
 	shooters_copy = copy.deepcopy(alive)
@@ -23,8 +25,8 @@ def get_best_shooter(shooters, shooter):
 		best_shooter = max(shooters_copy, key=shooters_copy.get)
 	return best_shooter
 
-def get_second_best_shooter(shooters, shooter):
-	'''Finds the most accurate living shooter who is not the person shooting. Returns the best shooter.'''
+def get_second_best_shooter(shooters, shooter): #shooter a string, shooters is a dict
+	'''Finds the most accurate living shooter who is not the person shooting. Returns the best shooter. Used in Strategy 3.'''
 	global alive
 	shooters_copy = copy.deepcopy(alive)
 	best_shooter = max(shooters_copy, key=shooters_copy.get) #find the best alive shooter
@@ -35,33 +37,41 @@ def get_second_best_shooter(shooters, shooter):
 	best_shooter = max(shooters_copy, key=shooters_copy.get) #find the second best alive shooter
 	return best_shooter
 
-def strategy1(shooters, shooter):
+def sort_shooters(shooters):
+	shooters = sorted(shooters.items(), key=operator.itemgetter(1)) #changes dictionary to list of tuples ordered by value
+	return shooters #list of tuples
+
+def strategy1(shooters, shooter): #shooter is a string, shooters is a dict
 	'''User-selected shooter will shoot in the air. The next shooter will shoot at the next most 
 	accurate shooter that remains alive and so one. Returns a list of survivors.'''
 	global alive
 	killed = []
 	#first two shots
 	#Right now, person 1 shoots first. to adjust, change slice
-	for person in list(shooters)[1:]: 
-		if person in alive: #if person is alive
+
+	#change this based on who shoots first
+	shooters = sort_shooters(shooters)
+
+	for person in shooters[1:]: #person is a tuple
+		if person[0] in alive: #if person is alive
 			r = random.uniform(0,1) #random number to compare to shooter's hit probability
 			best_shooter = get_best_shooter(shooters, person)
 
-			if alive[person] > r: #if shooter wins
+			if alive[person[0]] > r: #if shooter wins
 
 				killed.append(best_shooter) #removes best shooter from alive and appends to killed
 				del alive[best_shooter]
-
+	# print(shooters)
 	if len(killed) == 2:
 		return list(alive.keys())
 	else:
 		while len(killed) < 2 : #two people die, the game is over
-			for person in list(shooters): 
-				if person in alive: #if person is alive
+			for person in shooters: #person is a tuple
+				if person[0] in alive: #if person is alive
 					r = random.uniform(0,1) #random number to compare to shooter's hit probability
 					best_shooter = get_best_shooter(shooters, person)
 
-					if alive[person] > r: #if shooter wins
+					if alive[person[0]] > r: #if shooter wins
 						killed.append(best_shooter) #removes best shooter from alive and appends to killed
 						del alive[best_shooter]
 
@@ -74,39 +84,48 @@ def strategy2(shooters, shooter):
 	global alive
 	killed = []
 	
+	shooters = sort_shooters(shooters)
+
 	while len(killed) < 2 : #two people die, the game is over
 		for person in shooters: 
-			if person in alive: #if person is alive
+			if person[0] in alive: #if person is alive
+				# print('=========for loop===========')
+				# print(person[0])
 				r = random.uniform(0,1) #random number to compare to shooter's hit probability
-				best_shooter = get_best_shooter(shooters, person)
-
-				if alive[person] > r: #if shooter wins
-					print('person', person, 'shot', best_shooter)
+				if alive[person[0]] > r: #if shooter wins
+					best_shooter = get_best_shooter(shooters, person[0])
+					# print('=======best shooter=========', best_shooter)
+					# print(person[0], 'shot', best_shooter)
 					killed.append(best_shooter) #removes best shooter from alive and appends to killed
 					del alive[best_shooter]
 					# print(len(killed))
-
+	# print('$$$$$$$$$$ end round $$$$$$$$$$')
 	return list(alive.keys())
 
-def strategy3(shooters, shooter, victim):
+def strategy3(shooters, shooter):
+	'''User-selected shooter will shoot at second most accurate shooter.
+	Each shooter will shoot at the most accurate shooter that remains alive. Returns a list of survivors.'''
 	global alive
 	killed = []
+
+	shooters = sort_shooters(shooters)
+
 	#first two shots
 	#Right now, person 1 shoots first. to adjust, change slice
 	r = random.uniform(0,1) #random number to compare to shooter's hit probability
 	second_best_shooter = get_second_best_shooter(shooters, person)
 
-	if alive[person] > r: #if shooter wins
+	if alive[person[0]] > r: #if shooter wins
 		killed.append(second_best_shooter) #removes best shooter from alive and appends to killed
 		del alive[second_best_shooter]
 
 	while len(killed) < 2 : #two people die, the game is over
-		for person in list(shooters): 
-			if person in alive: #if person is alive
+		for person in shooters: 
+			if person[0] in alive: #if person is alive
 				r = random.uniform(0,1) #random number to compare to shooter's hit probability
 				best_shooter = get_best_shooter(shooters, person)
 
-				if alive[person] > r: #if shooter wins
+				if alive[person[0]] > r: #if shooter wins
 					killed.append(best_shooter) #removes best shooter from alive and appends to killed
 					del alive[best_shooter]
 
@@ -135,6 +154,7 @@ def conduct_experiment(n_experiments, strategy_choice, shooter):
 	i = 0
 
 	for i in range(0, n_experiments):
+		#reset shooters and alive to initial state
 		shooters = {'Shooter 1' : 1.0/3, 'Shooter 2' : 2.0/3, 'Shooter 3' : 1} #value = shooter accuracy
 		alive = copy.deepcopy(shooters)
 
@@ -145,13 +165,13 @@ def conduct_experiment(n_experiments, strategy_choice, shooter):
 			winner = strategy2(shooters, shooter)
 
 		if int(strategy_choice) == 3:
-			winner = strategy2(shooters, shooter)
+			winner = strategy3(shooters, shooter)
 
 		num_wins(winner)
 
 		plot_results = plot_results + winner
 
-		i += 1
+		i = i + 1
 
 	return plot_results
 
@@ -180,19 +200,24 @@ def main():
 	plot_results = [1 for x in plot_results if x == 'Shooter 1'] + [2 for x in plot_results if x == 'Shooter 2'] + [3 for x in plot_results if x == 'Shooter 3']
 	print(plot_results)
 	
-	for person in results:
-		print(person, results[person])
-	return results
 
-	bins = list(range(0, num_people + 1))
+	results = sorted(results.items())
+	print(results)#create list of tuples
+
+	for person in results: 
+		print(person[0], person[1])
+
+	print(results[1][1] + results[2][1])
+
+	bins = list(range(1, num_people + 2))
 
 	# #Probability
 	# a = plt.hist(results, bins, normed=1) #to calculate probability; also need to comment out plt.plot(bins) when using this line
 ##	plt.xlabel('Number of People Who Matched')
 ##	plt.ylabel('Probability')
 ##	plt.title('Histogram of Coat Check Experiment Probability, %d People' %num_people)	
-	average = sum(results)/float(len(results))
-	print('average', average)
+	# average = sum(results)/float(len(results))
+	# print('average', average)
 
 	#Experiment
 	plt.xlabel('Number of People Who Matched')
@@ -203,6 +228,9 @@ def main():
 	plt.hist(plot_results, bins)
 	plt.show()
 
+	# expected_bullets()
+
+	return results
 
 if __name__ == '__main__':
 	main()
